9fd60f8233eaf1cdf8f512cf97e6f839d3962ffc
imohsumov1 <imohsumov57@gmail.com>
2022-05-17 11:53:31 +0200

    adding apis

--- src/main/java/com/msexample/micro/controller/AnimalController.java
+++ src/main/java/com/msexample/micro/controller/AnimalController.java
@@ -5,10 +5,9 @@ import com.msexample.micro.services.AnimalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,10 +16,27 @@ public class AnimalController {
 
     private final AnimalService animalService;
 
-    @PostMapping("/animal")
+    @PostMapping("/animals")
     public ResponseEntity<String> add(@RequestBody Animal animal){
         Optional.ofNullable(animal).orElseThrow(NullPointerException::new);
         animalService.saveAnimal(animal);
         return new ResponseEntity<>("Added successfully", HttpStatus.OK);
     }
+
+    @GetMapping("/animals")
+    public List<Animal> getAll(){
+        return animalService.getAnimals();
+    }
+
+    @GetMapping("/animals/{id}")
+    public Animal getAnimalById(@PathVariable Long id){
+        return animalService.findById(id);
+    }
+
+    @DeleteMapping("/animals/{id}")
+    public void deleteById(@PathVariable Long id){
+        animalService.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/msexample/micro/services/AnimalService.java
+++ src/main/java/com/msexample/micro/services/AnimalService.java
@@ -3,7 +3,10 @@ package com.msexample.micro.services;
 import com.msexample.micro.entity.Animal;
 import com.msexample.micro.repository.AnimalRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,16 @@ public class AnimalService {
         animalRepository.save(animal);
     }
 
+    public List<Animal> getAnimals() {
+        return animalRepository.findAll();
+    }
+
+    public Animal findById(Long id) {
+        return animalRepository.findById(id).get();
+    }
+
+    public void deleteById(Long id) {
+        animalRepository.deleteById(id);
+    }
 }
 
